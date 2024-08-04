15e7dc3f8495f20a876280169d45b2ede80140b8
st-v7 <victorya_s@mail.ru>
2024-08-04 20:53:45 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -11,5 +11,20 @@ print(type(var_str))
 print(type(var_list))
 print(type(var_bool))
 
-#вызов функции
+# вызов функции
 task_1()
+
+
+def task_2():
+    a: list[int] = [1, 2, 3, 5, 8, 13, 21] Числа Фибоначчи
+print(a[:3])
+
+# вызов функции
+task_2()
+
+
+def task_3(number: int):
+    return number ** 2
+
+# вызов функции и распечатка результата
+print(task_3(5))  # пример вызова функции с числом 5
