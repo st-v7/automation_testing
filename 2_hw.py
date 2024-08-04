815c9e948d61ec4ee4176cc81349e02701520f62
st-v7 <victorya_s@mail.ru>
2024-08-04 20:37:37 +0300

    home_work_2_hw.py
    задача 1
    a. создайте функцию task_1(), в теле функции:
    i. создайте 5 разных переменных с произвольным названием.
    ii. у каждой укажите тип, типы следующие
    1. int, float, str, list, bool
    iii. заполните переменные произвольными значениями, с соответствующим для каждой типом
    iv. выведите тип данных каждой в консоль.
    b. в функцию добавьте аннотацию возвращаемых данных
    c. добавьте вызов функции.
    
    задача 2
    a. создайте функцию task_2(), в теле функции:
    i. есть список a = [1, 2, 3, 5, 8, 13, 21]
    ii. выведите в консоль первые 3 значения списка
    b. в функцию добавьте аннотацию возвращаемых данных
    c. добавьте вызов функции.
    d. * - скажите как называется эта последовательность чисел
    
    задача 3
    a. напишите функцию task_3(), которая принимает число и возвращает квадрат этого числа.
    b. пропишите аннотации для функции и аргумента
    c. распечатайте в консоль вызов функции

---
+++ 2_hw.py
@@ -0,0 +1,15 @@
+def task_1():
+    var_int: int = 7
+    var_float: float = 3.1415
+    var_str: str = "завершить гештальт"
+    var_list: list = [1, 2, 3, 4, 5]
+    var_bool: bool = True
+
+print(type(var_int))
+print(type(var_float))
+print(type(var_str))
+print(type(var_list))
+print(type(var_bool))
+
+#вызов функции
+task_1()
